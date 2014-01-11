5fe0b0792e99786b8abf238c68e52df98010e700
= <=>
2014-01-11 21:13:57 +0100

    Removed unused .fixed file. Made opencv be custom compiled.

--- install_kitrocopter.sh
+++ install_kitrocopter.sh
@@ -5,12 +5,25 @@ sudo service udev restart
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get -y install libgtk2.0-dev subversion git-core cmake freeglut3-dev pkg-config build-essential libxmu-dev libxi-dev libusb-1.0-0-dev ros-hydro-desktop libopencv-dev libopencv-core-dev libopencv-highgui-dev libopencv-features2d-dev libopencv-calib3d-dev libopencv-objdetect-dev
+sudo apt-get -y install libgtk2.0-dev subversion git-core cmake freeglut3-dev pkg-config build-essential libxmu-dev libxi-dev libusb-1.0-0-dev ros-hydro-desktop python-dev python-numpy libavcodec-dev libavformat-dev libswscale-dev libjpeg-dev libpng-dev
+
 mkdir /tmp/install
 cd /tmp/install
 
+# opencv
+
+git clone https://github.com/Itseez/opencv
+cd opencv
+git checkout 2.4.8
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make
+sudo make install
+cd ../..
+
 # cvblob
-svn checkout http://cvblob.googlecode.com/svn/trunk/ cvblob
+svn checkout -r 400 http://cvblob.googlecode.com/svn/trunk/ cvblob
 cd cvblob
 cmake .
 make
@@ -19,13 +32,15 @@ cd ..
 
 # libfreenect
 git clone git://github.com/OpenKinect/libfreenect.git
-git checkout c55f3c94c422afc9f4c3c34327da9526b44f32a2 # use correct version, so that our fix doesn't destroy anything.
 cd libfreenect
+git checkout c55f3c94c422afc9f4c3c34327da9526b44f32a2 # use correct version, so that our fix doesn't destroy anything.
 mkdir build
 cd build
 cmake ..
 make
 sudo make install
+
+sudo ln /usr/local/include/libfreenect/libfreenect.h /usr/local/include/libfreenect.h
 
 sudo ldconfig /usr/local/lib64/
 sudo adduser $USER video
@@ -37,7 +52,3 @@ echo "source /opt/ros/hydro/setup.bash" >> ~/.bashrc
 #source /opt/ros/hydro/setup.bash # needed for catkin_init_workspace
 source ~/.bashrc
 
-# add symbolic links to fix different directory structures
-sudo ln /usr/local/include/libfreenect/libfreenect.h /usr/local/include/libfreenect.h
-sudo ln -s -T /usr/include/opencv2 /usr/include/opencv
-sudo ln /usr/include/opencv2/core/core_c.h /usr/include/opencv2/cv.h
