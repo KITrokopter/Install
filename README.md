1499f41b696a8d3b85dd9b1e4a84732f8a2864e1
= <=>
2014-01-11 16:06:50 +0100

    For me, cvblob does compile... Uncommented cvblob stuff Fixed sourcing of ros setup.sh, is needed for another script so source directly

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Install
 =======
 
 Install scripts
+
+First install kitrocopter, then dev environment.
+
+You may be asked something about "hddtemp". No is just fine.
--- install_kitrocopter.sh
+++ install_kitrocopter.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 sudo cp ./etc/51-kinect.rules /etc/udev/rules.d/51-kinect.rules
+sudo service udev restart
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install libopencv-dev libopencv-core-dev libgtk2.0-dev libopencv-highgui-dev libopencv-features2d-dev libopencv-calib3d-dev subversion libopencv-objdetect-dev git-core cmake freeglut3-dev pkg-config build-essential libxmu-dev libxi-dev libusb-1.0-0-dev subversion ros-hydro-desktop-full
 mkdir /tmp/install
 cd /tmp/install
-#cvblob does not compile yet :(
-#svn checkout http://cvblob.googlecode.com/svn/trunk/ cvblob
-#cd cvblob
-#cmake .
-#make
-#sudo make install
+svn checkout http://cvblob.googlecode.com/svn/trunk/ cvblob
+cd cvblob
+cmake .
+make
+sudo make install
+cd ..
 git clone git://github.com/OpenKinect/libfreenect.git
 cd libfreenect
 mkdir build
@@ -26,7 +27,6 @@ sudo rm -r /tmp/install
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/hydro/setup.bash" >> ~/.bashrc
+source /opt/ros/hydro/setup.bash # needed for catkin_init_workspace
 source ~/.bashrc
 
-#rosinstall
-#sudo apt-get install python-rosinstall
