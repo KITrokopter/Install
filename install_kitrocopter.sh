d64f41db9e0cb46007fa318f1442f15d9dd8ed29
= <=>
2014-01-11 19:37:00 +0100

    Removed duplicate subversion Don't install ros full, just desktop Remove dirty fix for libfreenect
    Add links for libfreenect and opencv includes

--- install_kitrocopter.sh
+++ install_kitrocopter.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # store current dir
 sudo cp ./etc/51-kinect.rules /etc/udev/rules.d/51-kinect.rules
 sudo cp ./etc/lib64.conf /etc/ld.so.conf.d/
 sudo service udev restart
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install libopencv-dev libopencv-core-dev libgtk2.0-dev libopencv-highgui-dev libopencv-features2d-dev libopencv-calib3d-dev subversion libopencv-objdetect-dev git-core cmake freeglut3-dev pkg-config build-essential libxmu-dev libxi-dev libusb-1.0-0-dev subversion ros-hydro-desktop-full
+sudo apt-get -y install libgtk2.0-dev subversion git-core cmake freeglut3-dev pkg-config build-essential libxmu-dev libxi-dev libusb-1.0-0-dev ros-hydro-desktop libopencv-dev libopencv-core-dev libopencv-highgui-dev libopencv-features2d-dev libopencv-calib3d-dev libopencv-objdetect-dev
 mkdir /tmp/install
 cd /tmp/install
+
+# cvblob
 svn checkout http://cvblob.googlecode.com/svn/trunk/ cvblob
 cd cvblob
 cmake .
 make
 sudo make install
 cd ..
+
+# libfreenect
 git clone git://github.com/OpenKinect/libfreenect.git
 git checkout c55f3c94c422afc9f4c3c34327da9526b44f32a2 # use correct version, so that our fix doesn't destroy anything.
 cd libfreenect
@@ -23,6 +26,7 @@ cd build
 cmake ..
 make
 sudo make install
+
 sudo ldconfig /usr/local/lib64/
 sudo adduser $USER video
 glview
@@ -32,5 +36,8 @@ rosdep update
 echo "source /opt/ros/hydro/setup.bash" >> ~/.bashrc
 #source /opt/ros/hydro/setup.bash # needed for catkin_init_workspace
 source ~/.bashrc
-cd $DIR
-sudo cp etc/libfreenect.hpp.fixed /usr/local/include/freenect.hpp # fix wrong include in freenect.hpp
+
+# add symbolic links to fix different directory structures
+sudo ln /usr/local/include/libfreenect/libfreenect.h /usr/local/include/libfreenect.h
+sudo ln -s -T /usr/include/opencv2 /usr/include/opencv
+sudo ln /usr/include/opencv2/core/core_c.h /usr/include/opencv2/cv.h
