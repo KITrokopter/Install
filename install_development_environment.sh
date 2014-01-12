144df0349919ad6f4404e2c215413950e8d8e763
Luchs <Luchs@euirc.eu>
2014-01-12 12:55:55 +0100

    Add -y to apt-get install

--- install_development_environment.sh
+++ install_development_environment.sh
@@ -5,15 +5,19 @@ set -eu
 sudo apt-get update
 sudo cp ./etc/cfclient.desktop /usr/share/applications
 sudo cp ./etc/cfclient.png /usr/share/icons
-sudo apt-get install python2.7 python-usb python-pygame python-qt4 mercurial qtcreator cmake qt4-qmake 
+sudo apt-get install -y python2.7 python-usb python-pygame python-qt4 mercurial qtcreator cmake qt4-qmake
+
 mkdir /tmp/install
 cd /tmp/install
 hg clone https://bitbucket.org/bitcraze/crazyflie-pc-client/src
+
 cd src
 sudo sh ./setup.sh
+
 cd ../../
 sudo rm -r install
 mkdir -p ~/ros_ws/src
+
 cd ~/ros_ws/src
 catkin_init_workspace
 git clone https://github.com/KITrokopter/Quadcopter-Application
