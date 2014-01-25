2e695e3a3f4cfb18e31867b3d9ad36b0b43263f4
ISibboI <isibboi@gmail.com>
2014-01-25 01:02:33 +0100

    Update install_matlab_redirects.sh

--- install_matlab_redirects.sh
+++ install_matlab_redirects.sh
@@ -12,4 +12,4 @@ cd /tmp/install
 echo "$USER_HOME/matlab/bin/glnxa64/" > matlab.conf
 sudo cp matlab.conf /etc/ld.so.conf.d/
 
-echo "PATH=$PATH:$USER_HOME/matlab/bin" >> ~/.bashrc
+echo "export PATH=$PATH:$USER_HOME/matlab/bin" >> ~/.bashrc
