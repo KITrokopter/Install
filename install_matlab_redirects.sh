cf767ab7b4db086ac15c0db9d0780690fa7cd8d0
Daniela Grimm <grimmdani93@googlemail.com>
2014-01-27 21:12:17 +0100

    Removed adding matlab library path to ldconfig, since it destroys the system.

--- install_matlab_redirects.sh
+++ install_matlab_redirects.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-[ whoami = "root" ] && echo "Don't run me as root!" && exit
-
-USER_HOME=$HOME
-
-[ -d /tmp/install ] && rm -rf /tmp/install
-mkdir -p /tmp/install
-cd /tmp/install
-
-
-echo "$USER_HOME/matlab/bin/glnxa64/" > matlab.conf
-sudo cp matlab.conf /etc/ld.so.conf.d/
-
 echo "PATH=$PATH:$USER_HOME/matlab/bin" >> ~/.bashrc
