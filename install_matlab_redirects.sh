83db6f822e38f703137883393e244afc5388bc56
ISibboI <isibboi@gmail.com>
2014-02-18 17:46:13 +0100

    Update install_matlab_redirects.sh
    Added libopencv_highgui / libtiff4 fix

--- install_matlab_redirects.sh
+++ install_matlab_redirects.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "export PATH=$PATH:$USER_HOME/matlab/bin" >> ~/.bashrc
+
+# Required to use matlab and libopencv_highgui at once
+cd $USER_HOME
+mkdir .matlabbackup
+cd matlab/bin/glnxa64
+mv libtiff5* ../../.matlabbackup
+
+echo "Moved libtiff5 to ~/.matlabbackup"
